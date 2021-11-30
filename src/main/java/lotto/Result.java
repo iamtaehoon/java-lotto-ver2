6675d44891021bf15fb88cc44b59fe1e21f91c36
iamtaehoon <kimth9981@naver.com>
2021-11-30 21:07:35 +0900

    refactor: Rank 단계별로 개수 초기화 로직 stream 사용해 변경

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import view.OutPutView;
@@ -7,12 +8,7 @@ import view.OutPutView;
 public class Result {
 	HashMap<Rank, Integer> rankOfResult = new HashMap<>();
 	public Result() { //TODO 뭔가 코드를 단순하게 만들 수 있지 않을까?
-		rankOfResult.put(Rank.FIRST, 0);
-		rankOfResult.put(Rank.SECOND, 0);
-		rankOfResult.put(Rank.THIRD, 0);
-		rankOfResult.put(Rank.FOURTH, 0);
-		rankOfResult.put(Rank.FIFTH, 0);
-		rankOfResult.put(Rank.LOSE, 0);
+		Arrays.stream(Rank.values()).forEach(rankLevel -> rankOfResult.put(rankLevel, 0));
 	}
 
 	public void addRank(Rank rank) {
