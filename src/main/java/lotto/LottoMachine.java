6ceee6f78365879cdde19d49e3440c246876444b
iamtaehoon <kimth9981@naver.com>
2021-11-30 21:04:47 +0900

    refactor: LottoMachine 클래스 내부 메서드 가독성 향상

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -9,6 +9,7 @@ public class LottoMachine {
 	ArrayList<LottoTicket> lottoTickets;
 	ArrayList<Integer> winningNum = new ArrayList<>();
 	Result result = new Result();
+	int bonusBall;
 
 	public LottoMachine(ArrayList<LottoTicket> lottoTickets) {
 		this.lottoTickets = lottoTickets;
@@ -27,22 +28,41 @@ public class LottoMachine {
 
 	public void getResult() {
 		String[] inputWinningNum = InputView.enterWinningNum();
-		int bonusBall = InputView.enterBonusBall();
-		validateWinningNumAndBonusBall(inputWinningNum, bonusBall);
+		validateWinningNum(inputWinningNum);
 		makeWinningNum(inputWinningNum);
+		compareAllPurchasedTicketAndWinningNum();
+		result.getResult();
+
+	}
+
+	private void compareAllPurchasedTicketAndWinningNum() {
+		bonusBall = InputView.enterBonusBall();
+		validateBonusBall(bonusBall);
+		compareEachTicketAndWinningNum();
+	}
+
+	private void compareEachTicketAndWinningNum() {
 		for (LottoTicket lottoTicket : lottoTickets) {
 			int matchingCnt = lottoTicket.compareWinningNum(winningNum);
 			boolean matchBonusBall = lottoTicket.hasBonusBall(bonusBall);
 			Rank rank = Rank.valueOf(matchingCnt, matchBonusBall);
 			result.addRank(rank);
 		}
-		result.getResult();
-
 	}
 
-	private void validateWinningNumAndBonusBall(String[] inputWinningNum, int bonusBall) {
-		new LottoTicket(inputWinningNum); // 범위 제대로 들어가고 겹치지도 않았고
+	private void validateBonusBall(int bonusBall) {
 		new LottoBall(bonusBall);
+		cantOverlapBonusBallAnyWinningNumDigit(bonusBall);
+	}
+
+	private void cantOverlapBonusBallAnyWinningNumDigit(int bonusBall) {
+		if (winningNum.stream().anyMatch(winningNumDigit -> winningNumDigit == bonusBall)) {
+			throw new IllegalArgumentException("보너스볼이 당첨 번호의 숫자와 겹칩니다.");
+		}
+	}
+
+	private void validateWinningNum(String[] inputWinningNum) {
+		new LottoTicket(inputWinningNum);
 	}
 
 	private void makeWinningNum(String[] inputWinningNum) {
--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -5,18 +5,15 @@ import java.util.HashMap;
 import view.OutPutView;
 
 public class Result {
-	HashMap<Rank, Integer> rankOfResult;
-	public Result() {
-		rankOfResult = new HashMap<>();
+	HashMap<Rank, Integer> rankOfResult = new HashMap<>();
+	public Result() { //TODO 뭔가 코드를 단순하게 만들 수 있지 않을까?
 		rankOfResult.put(Rank.FIRST, 0);
 		rankOfResult.put(Rank.SECOND, 0);
 		rankOfResult.put(Rank.THIRD, 0);
 		rankOfResult.put(Rank.FOURTH, 0);
 		rankOfResult.put(Rank.FIFTH, 0);
 		rankOfResult.put(Rank.LOSE, 0);
-
 	}
-
 
 	public void addRank(Rank rank) {
 		rankOfResult.put(rank, rankOfResult.get(rank) + 1);
